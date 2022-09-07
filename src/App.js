cea43d49e547540d645eda0774fcd8928a0497a1
Fabio Leandro <fabiolpsolucoes@gmail.com>
2022-09-07 14:53:14 -0300

    Listando titulos e capas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import MovieRow from './components/movierow/MovieRow';
 import Tmdb from './tmdb/Tmdb';
 
 function App() {
 
+  const [movieList,setMovieList] = useState([]);
+
   useEffect(()=>{
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
-      console.log(list)
+      setMovieList(list)
     }
 
     loadAll()
@@ -15,8 +18,12 @@ function App() {
 
 
   return (
-    <div className="App">
-      <p>HELLO WORLD</p>
+    <div className="page">
+      <section className='lists'>
+        {movieList.map((item,key) => (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
     </div>
   );
 }
